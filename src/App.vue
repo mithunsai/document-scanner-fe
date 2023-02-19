e9b8a2fc3b36f532d21c92e8d97bc5426365992b
Mithun M <mmithunsai@gmail.com>
2023-02-19 16:08:16 +0530

    creates starter

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ImageUpload from "./views/ImageUpload.vue";
+</script>
+<template>
+  <p>Hello</p>
+  <ImageUpload></ImageUpload>
+</template>
---
+++ src/views/ImageUpload.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <form action="" >
+            <input type="file" multiple>
+        </form>
+     
+        
+    </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+
+
+export default {
+    setup(){
+    }
+}
+</script>
