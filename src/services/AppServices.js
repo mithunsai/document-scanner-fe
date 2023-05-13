e6568c8dc99282e1593fff66e111c34e5aabb3bc
Mithun M <mmithunsai@gmail.com>
2023-05-14 01:03:33 +0530

    adds proxy

--- src/services/AppServices.js
+++ src/services/AppServices.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const appServices = {
   uploadImages: (imageFiles) => {
-    const url = "http://127.0.0.1:8000/uploadImages/";
+    const url = "/uploadImages/";
     return axios.post(url, imageFiles, {
       headers: {
         "Content-Type": "multipart/form-data",
--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server:{
+    proxy:{
+      '/uploadImages/':"https://document-scanner-be.onrender.com"
+    }
   }
 })
