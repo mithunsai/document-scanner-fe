4205e68d4f16627157b36227e28798076fa318cc
Mithun M <mmithunsai@gmail.com>
2023-05-14 21:19:38 +0530

    updates the height and width of the elements and incorporates flex

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 </script>
 <template>
-  <div class="container-fluid">
+  <div class="h-100 w-100 d-flex flex-column">
       <Header></Header>
-      <ImgToPdf></ImgToPdf>
-      <Footer></Footer>    
+      <ImgToPdf class="flex-fill"></ImgToPdf>
+      <Footer class=""></Footer>    
   </div>
 </template>
